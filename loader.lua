1b9e005a9b3b350f8f3a0018a8d0466c84fffecb
eo-eo-eo <eduardosipov13@outlook.com>
2025-05-27 20:07:31 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -9,5 +9,5 @@ local localPlayer = game:GetService("Players").LocalPlayer
 if table.find(allowedUsers, localPlayer.Name) then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/eo-eo-eo/fuckoff/refs/heads/main/main.lua"))()
 else
-    game:GetService("TeleportService"):Teleport(72028966925446)
+    game:GetService("TeleportService"):Teleport(106719208264278)
 end
