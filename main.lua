eb2f4b960b9079d5a3ef47e095d0c31e98ee3f07
eo-eo-eo <eduardosipov13@outlook.com>
2025-06-01 21:13:05 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -29,9 +29,9 @@ local function createButton(text, y, parent, action)
 end
 
 if game.PlaceId == 117452115137842 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/eo-eo-eo/fuckoff/refs/heads/main/main.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/eo-eo-eo/fuckoff/refs/heads/main/lobby.lua"))()
 end
 
 if game.PlaceId == 83363871432855 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/eo-eo-eo/fuckoff/refs/heads/main/main.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/eo-eo-eo/fuckoff/refs/heads/main/ingame.lua"))()
 end
