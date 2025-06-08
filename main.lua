405f81f7eabe9561bc112dc50814467c9f5d9c21
eo-eo-eo <eduardosipov13@outlook.com>
2025-06-08 22:00:09 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -67,6 +67,6 @@ end
 
 if game.PlaceId == 117452115137842 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/eo-eo-eo/fuckoff/refs/heads/main/lobby.lua"))()
-elseif game.PlaceId == 83363871432855 then
+if game.PlaceId == 83363871432855 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/eo-eo-eo/fuckoff/refs/heads/main/ingame.lua"))()
 end
